831d135f59c32f08063e9747b4a94fb5adc00a3b
Yuji181181 <s1f102402552@iniad.org>
2024-11-29 15:18:46 +0900

    view 編集

--- CS2-exercise/django1/forecast/views.py
+++ CS2-exercise/django1/forecast/views.py
@@ -3,6 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 
 def index(request):
-    return render(request, 'forecast/index.html')
-
-
+    params = {}
+    params['title'] = '3 days forecast'
+    return render(request, 'forecast/index.html', params)
