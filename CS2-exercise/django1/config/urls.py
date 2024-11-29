a85cf20825f8ba1011b4b599eb031d8f2d09d403
Yuji181181 <s1f102402552@iniad.org>
2024-11-29 15:14:02 +0900

    urls 編集

--- CS2-exercise/django1/config/urls.py
+++ CS2-exercise/django1/config/urls.py
@@ -17,6 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 import app01.views
+import forecast.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -24,4 +25,5 @@ urlpatterns = [
     path('', app01.views.root, name='root'),
     path('app01/pattern/<username>/', app01.views.pattern, name='pattern'),
     path('app01/param/', app01.views.param, name='param'),
+    path('forecast/', forecast.views.index, name='forecast'),
 ]
