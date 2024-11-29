eefa5473700a670833710d0a777d7ba0a945b01e
Yuji181181 <s1f102402552@iniad.org>
2024-11-29 15:28:03 +0900

    ramdom

--- CS2-exercise/django1/forecast/views.py
+++ CS2-exercise/django1/forecast/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
+import random
 
 # Create your views here.
 
+weathers = ['Sunny', 'Rainy', 'Cloudy', 'Snowy']
 def index(request):
     params = {}
     params['title'] = '3 days forecast'
+    params['forecast'] = random.choices(weathers, k=3)
     return render(request, 'forecast/index.html', params)
