5d7cc22ecd615d0fe808f0ace386324ab2129171
Yuji181181 <s1f102402552@iniad.org>
2024-12-05 15:45:11 +0900

    おみくじ

--- CS2-exercise/blog_sample/blog/views.py
+++ CS2-exercise/blog_sample/blog/views.py
@@ -10,11 +10,13 @@ def update(request, article_id):
 	return HttpResponse("article_id: {}".format(article_id))
 
 def hello(request):
-    data = {
+	great_message_list = ["Great Fortune", "Small Fortune", "Bad Fortune"]
+	random_number = random.randint(0, 2)
+	data = {
 		'name': 'Alice',
 		'weather': 'CLOUDY',
 		'weather_detail':['Temperature:23℃','Humidity:40%','Wind: 5m/s'],
-		'isGreatFortune':True,
-		'fortune': 'Great Fortune!'
+		'isGreatFortune':random_number == 0,
+		'fortune': great_message_list[random_number]
 	}
-    return render(request, 'blog/hello.html',data)
+	return render(request, 'blog/hello.html',data)
