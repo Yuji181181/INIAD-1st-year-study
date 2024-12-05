bf0559d940026ec4a6cbd3e834ea1692342d4320
Yuji181181 <s1f102402552@iniad.org>
2024-12-05 15:47:25 +0900

    urls redirect

--- CS2-exercise/blog_sample/blog/urls.py
+++ CS2-exercise/blog_sample/blog/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
 	path('', views.index, name='index'),
     path('<int:article_id>/update', views.update, name='update'),
-    path('hello', views.hello, name='hello')
+    path('hello', views.hello, name='hello'),
+    path('redirect', views.redirect_test, name='redirect_test'),
 ]
