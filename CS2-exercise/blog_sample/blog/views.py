7c84ef76e69f3fe631f47ae6ce463bb7d06372d7
Yuji181181 <s1f102402552@iniad.org>
2024-12-05 15:56:31 +0900

    def detail update delete

--- CS2-exercise/blog_sample/blog/views.py
+++ CS2-exercise/blog_sample/blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 import random
 
@@ -6,8 +6,20 @@ import random
 def index(request):
 	return render(request, 'blog/index.html')
 
+def detail(request, article_id):
+	context = {
+		'article_id': article_id
+	}
+	return render(request, 'blog/tbd.html', context)
+
 def update(request, article_id):
-	return HttpResponse("article_id: {}".format(article_id))
+	context = {
+		'article_id': article_id
+	}
+	return render(request, 'blog/tbd.html', context)
+
+def delete(request, article_id):
+	return redirect(index)
 
 def hello(request):
 	great_message_list = ["Great Fortune", "Small Fortune", "Bad Fortune"]
@@ -18,5 +30,8 @@ def hello(request):
 		'weather_detail':['Temperature:23℃','Humidity:40%','Wind: 5m/s'],
 		'isGreatFortune':random_number == 0,
 		'fortune': great_message_list[random_number]
-	}
+	}	
 	return render(request, 'blog/hello.html',data)
+
+def redirect_test(request):
+	return redirect(hello)
