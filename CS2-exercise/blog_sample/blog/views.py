4ae93ba5a603114a1fede9fa9e8aba06d543d5d2
Yuji181181 <s1f102402552@iniad.org>
2024-12-05 16:00:18 +0900

    timezone

--- CS2-exercise/blog_sample/blog/views.py
+++ CS2-exercise/blog_sample/blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 import random
+from django.utils import timezone
 
 # Create your views here.
 def index(request):
