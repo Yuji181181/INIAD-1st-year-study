87b8707f22eb67cc4948657aa1f950dd2d616ca0
Yuji181181 <s1f102402552@iniad.org>
2024-12-09 16:27:51 +0900

    models

--- CS2-exercise/blog_sample/blog/views.py
+++ CS2-exercise/blog_sample/blog/views.py
@@ -3,47 +3,39 @@ from django.http import HttpResponse
 import random
 from django.utils import timezone
 
+
 # Create your views here.
 def index(request):
-    context = {
-        "articles": [
-            {
-                "id": 1,
-                "title": "Post 01",
-                "body": "test post.\nLorem ipsum dolor sit amet, \nconsectetur adipiscing elit,\n sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.\n", 
-                "posted_at": timezone.now # option
-            }
-        ]
-    }
-    return render(request, 'blog/index.html',context)
+    context = {"articles": Article.objects.all()}
+    return render(request, "blog/index.html", context)
 
 
 def detail(request, article_id):
-	context = {
-		'article_id': article_id
-	}
-	return render(request, 'blog/tbd.html', context)
+    context = {"article_id": article_id}
+    return render(request, "blog/tbd.html", context)
+
 
 def update(request, article_id):
-	context = {
-		'article_id': article_id
-	}
-	return render(request, 'blog/tbd.html', context)
+    context = {"article_id": article_id}
+    return render(request, "blog/tbd.html", context)
+
 
 def delete(request, article_id):
-	return redirect(index)
+    return redirect(index)
+
 
 def hello(request):
-	great_message_list = ["Great Fortune", "Small Fortune", "Bad Fortune"]
-	random_number = random.randint(0, 2)
-	data = {
-		'name': 'Alice',
-		'weather': 'CLOUDY',
-		'weather_detail':['Temperature:23℃','Humidity:40%','Wind: 5m/s'],
-		'isGreatFortune':random_number == 0,
-		'fortune': great_message_list[random_number]
-	}	
-	return render(request, 'blog/hello.html',data)
+    great_message_list = ["Great Fortune", "Small Fortune", "Bad Fortune"]
+    random_number = random.randint(0, 2)
+    data = {
+        "name": "Alice",
+        "weather": "CLOUDY",
+        "weather_detail": ["Temperature:23℃", "Humidity:40%", "Wind: 5m/s"],
+        "isGreatFortune": random_number == 0,
+        "fortune": great_message_list[random_number],
+    }
+    return render(request, "blog/hello.html", data)
+
 
 def redirect_test(request):
-	return redirect(hello)
+    return redirect(hello)
