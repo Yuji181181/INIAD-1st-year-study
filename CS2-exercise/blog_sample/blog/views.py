65d0b2a5b3fdeec5f987bf7b0fad67205ac71eae
Yuji181181 <s1f102402552@iniad.org>
2024-12-16 15:15:26 +0900

    記事削除機能を追加し、詳細ページに削除リンクを追加

--- CS2-exercise/blog_sample/blog/views.py
+++ CS2-exercise/blog_sample/blog/views.py
@@ -8,6 +8,10 @@ from blog.models import Article
 
 # Create your views here.
 def index(request):
+    if request.method == "POST":
+        article = Article(title=request.POST["title"], body=request.POST["text"])
+        article.save()
+        return redirect(detail, article.id)
     context = {"articles": Article.objects.all()}
     return render(request, "blog/index.html", context)
 
@@ -31,6 +35,11 @@ def update(request, article_id):
 
 
 def delete(request, article_id):
+    try:
+        article = Article.objects.get(pk=article_id)
+    except Article.DoesNotExist:
+        raise Http404("Article does not exist")
+    article.delete()
     return redirect(index)
 
 
